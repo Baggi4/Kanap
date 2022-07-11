03480417df7b1fabc06d14b2e1d42ad45b0ad41c
Ste <74907151+Baggi4@users.noreply.github.com>
2022-07-12 01:50:09 +0200

    Delete front/js directory

--- front/js/confirmation.js
+++
@@ -1,8 +0,0 @@
-//Affichage de l'idOrder dans la page de confirmation
-const paramsUrl = new URLSearchParams(document.location.search);
-const productCart = JSON.parse(localStorage.getItem("produits"));
-
-//Return les paramètres idOrder de URL
-const parametre = paramsUrl.get("id");
-const idOrders = document.getElementById("orderId");
-idOrders.textContent = parametre;
