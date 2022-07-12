5e87352f8dae85b096a738770cba64927a003838
Ste <74907151+Baggi4@users.noreply.github.com>
2022-07-12 15:02:17 +0200

    Delete front/js directory

--- front/js/confirmation.js
+++
@@ -1,5 +0,0 @@
-const paramsUrl = new URLSearchParams(document.location.search);
-const productCart = JSON.parse(localStorage.getItem("produits"));
-const parametre = paramsUrl.get("id");
-const idOrders = document.getElementById("orderId");
-idOrders.textContent = parametre;
