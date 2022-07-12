e33ca00a5127c14fb3d75f2d75ceceb8b83e92e2
Ste <74907151+Baggi4@users.noreply.github.com>
2022-07-12 16:47:55 +0200

    Delete front directory

--- front/js/confirmation.js
+++
@@ -1,8 +0,0 @@
-// Afficher le numéro de commande
-function confirmOrder() {
-  const numberID = new URLSearchParams(document.location.search);
-  let id = numberID.get("id");
-  console.log(id);
-  document.getElementById("orderId").innerHTML = id;
-}
-confirmOrder();
